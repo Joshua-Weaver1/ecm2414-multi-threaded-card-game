adcf3d499e90d9bfb3ee18f704b8f849c6c18eaa
Joshua <joshua.weaverr@gmail.com>
2022-11-02 21:02:35 +0000

    Changes to be committed: 	modified:   src/Card.java 	modified:   src/CardGame.java

--- src/Card.java
+++ src/Card.java
@@ -1,3 +1,23 @@
+/**
+ * This is the class for a card.
+ *
+ * @author Kevin Liu & Joshua Weaver
+ * @version 1.0
+ */
+
 public class Card {
-    
+    private final int cardNumber;
+
+    public Card(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "" + cardNumber;
+    }
 }
--- src/CardGame.java
+++ src/CardGame.java
@@ -1,7 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * This is the executable CardGame class.
+ * 
+ * @author Kevin Liu & Joshua Weaver
+ * @version 1.0
+ */
+
 public class CardGame {
     public static void main(String args[]) {
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        try {
+            BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter the number of desired players here: ");
+            int n = Integer.parseInt(bReader.readLine());
+            System.out.println("Enter the filename of the desired pack here: ");
+            String filename = bReader.readLine();
+
+
+        } catch (IOException e) {
+            System.out.println("exception: " + e);
+        }
     }
 }
