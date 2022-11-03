b9859fb4e1f4f8cad537e4dd0a66e7c85453e8f7
Joshua <joshua.weaverr@gmail.com>
2022-11-03 17:40:43 +0000

    Created Pack Class

--- src/CardGame.java
+++ src/CardGame.java
@@ -10,17 +10,26 @@ import java.io.InputStreamReader;
  */
 
 public class CardGame {
+
     public static void main(String args[]) {
+
         try {
             BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
             System.out.println("Enter the number of desired players here: ");
-            int n = Integer.parseInt(bReader.readLine());
+            int x = Integer.parseInt(bReader.readLine());
             System.out.println("Enter the filename of the desired pack here: ");
-            String filename = bReader.readLine();
+            String nameOfFile = bReader.readLine();
+
+            System.out.println(x);
+            System.out.println(nameOfFile);
 
 
         } catch (IOException e) {
-            System.out.println("exception: " + e);
+            System.out.println("Error: " + e);
         }
+    }
+
+    public static void startSimulation(String nameOfFile, int x) throws IOException {
+        Pack packOfCards = new Pack(x, nameOfFile);
     }
 }
--- src/Pack.java
+++ src/Pack.java
@@ -1,3 +1,28 @@
+import java.io.IOException;
+
+/**
+ * Pack class.
+ * 
+ * @author Kevin Liu & Joshua Weaver
+ * @version 1.0
+ */
+
 public class Pack {
-    
+    //Assigning new pack of cards variable to Pack
+    private Card[] packOfCards;
+
+    public Card[] getPackOfCards() {
+        return packOfCards;
+    }
+
+    /**
+     * Constructor
+     * 
+     * @param x
+     * @param nameOfFile
+     * @throws IOException
+     */
+    public Pack(int x, String nameOfFile) throws IOException {
+        this.packOfCards = new Card[x * 8];
+    }
 }
