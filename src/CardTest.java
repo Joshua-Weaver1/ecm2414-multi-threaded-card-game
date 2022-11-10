4b4c121d1ebccba152f5334ef119e1bb811e956d
Joshua <joshua.weaverr@gmail.com>
2022-11-10 16:00:40 +0000

    Added test to CardTest

--- src/CardTest.java
+++ src/CardTest.java
@@ -1,8 +1,24 @@
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CardTest {
+    Card testCard;
+    int testCardValue;
+
+    @Before
+    public void initialise() {
+        this.testCardValue = (int) Math.round(Math.random() * 100);
+        this.testCard = new Card(this.testCardValue);
+    }
+
+    @Test
+    public void getCardNumber() {
+        assertEquals(this.testCard.getCardNumber(), testCardValue);
+    }
+
+
     @Test
     public void test() {
         assertEquals("Hello!", "Hello!");
