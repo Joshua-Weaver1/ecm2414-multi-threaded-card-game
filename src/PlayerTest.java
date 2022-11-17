be60e4979b343ed18700e23e11ddf23e23ad6e47
Kevin Liu <kl564@exeter.ac.uk>
2022-11-17 16:25:04 +0000

    changes

--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -4,7 +4,9 @@ import org.junit.Test;
 
 public class PlayerTest {
     @Test
-    public void test() {
-        assertEquals("Hello!", "Hello!");
+    public void getPlayerIdTest() {
+        Player player = new Player(1);
+        assertEquals("Incorrect player id", 1, player.getPlayerId());
     }
+    
 }
